7c35a52bbd30374980ef3f3a892f4989b1b64821
sharmina-lina <lina_cse02@yahoo.com>
2024-11-04 09:17:46 +0100

    app.test change

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  screen.debug();
+  const linkElement = screen.findByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
