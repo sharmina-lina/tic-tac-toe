7c7fd6ef0820be3a80539cfd1c170c0176f2fdc7
sharmina-lina <lina_cse02@yahoo.com>
2024-11-04 11:14:51 +0100

    Fix test to check for Tic Tac Toe heading

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Tic Tac Toe heading', async () => {
   render(<App />);
-  screen.debug();
-  const linkElement = screen.findByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = await screen.findByText(/tic tac toe/i);
+  expect(headingElement).toBeInTheDocument();
 });
+
+
